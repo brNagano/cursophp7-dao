3a19f112cef12f83b2ff4ff578c563dca59a5782
unknown <b.nagano22@outlook.com>
2018-08-29 15:07:14 -0300

    Métodos de lista

--- index.php
+++ index.php
@@ -2,9 +2,22 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+// Traz um usuário
+/*$root = new Usuario();
 $root->loadById(4);
+echo $root;*/
 
-echo $root;
+// Traz uma lista de usuários
+/*$lista = Usuario::getList();
+echo json_encode($lista);*/
+
+// Traz uma lista de usuários buscando pelo login
+/*$search = Usuario::search("o");
+echo json_encode($search);*/
+
+// Traz usuário validando pelo login e senha
+$usuario = new Usuario();
+$usuario->login("user","12345");
+echo $usuario;
 
 ?>
